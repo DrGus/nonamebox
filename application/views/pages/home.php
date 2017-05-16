b5f56067eabd965d075165a2d531b47b68fdb7d8
Agustí Montes <amontes@dowhilestudio.com>
2017-05-16 09:36:49 +0200

    Update with testing website.

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -9,6 +9,7 @@
     <?php/* STYLES CSS */ ?>
     <link rel="stylesheet" href="<?=public_url()?>res/bootstrap/css/bootstrap.min.css"/>
     <link rel="stylesheet" href="<?=public_url()?>res/bootstrap/css/bootstrap-theme.min.css"/>
+    <link rel="stylesheet" href="<?=public_url()?>css/nnb-core.css"/>
     <link rel="stylesheet" href="<?=public_url()?>css/nnb-frontstyle.css"/>
     <?php/* STYLES CSS */ ?>
   </head>
@@ -47,17 +48,38 @@
       </div><!-- nav/.container -->
     </nav>
 
-    <div class="container nnb-page-content">
-      <div class="starter-template">
-        <h1>Hello World!</h1>
-        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum facilisis aliquet sodales. Nulla massa lacus, scelerisque sodales consectetur et, vestibulum non diam. Nulla ullamcorper eros non sem auctor mattis. Nullam faucibus lacinia lectus, ac consectetur est sagittis quis. Nullam sit amet sapien justo. In in lacinia quam, at aliquet ligula. Proin turpis lectus, finibus viverra nunc mollis, dignissim venenatis quam. Nulla et scelerisque ipsum. Donec aliquam aliquet elit eu ullamcorper. Nulla facilisi. Vestibulum mollis lacus ipsum, non accumsan arcu consectetur nec. Cras ante erat, consequat consequat mattis eget, aliquam sed dui. Ut sollicitudin tempus orci sed consequat. </p>
-      </div>
+    <div class="row">
+      <div class="container nnb-page-content">
+        <div class="starter-template">
+          <h1>No Name Box</h1>
+          <h2>Account availability</h2>
+          <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum facilisis aliquet sodales. Nulla massa lacus, scelerisque sodales consectetur et, vestibulum non diam. Nulla ullamcorper eros non sem auctor mattis. Nullam faucibus lacinia lectus, ac consectetur est sagittis quis. Nullam sit amet sapien justo. In in lacinia quam, at aliquet ligula. Proin turpis lectus, finibus viverra nunc mollis, dignissim venenatis quam. Nulla et scelerisque ipsum. Donec aliquam aliquet elit eu ullamcorper. Nulla facilisi. Vestibulum mollis lacus ipsum, non accumsan arcu consectetur nec. Cras ante erat, consequat consequat mattis eget, aliquam sed dui. Ut sollicitudin tempus orci sed consequat. </p>
+        </div>
+      </div><!-- /.container -->
+    </div><!-- /.row -->
 
-    </div><!-- /.container -->
+    <div class="row nnb-greenbgcolor">
+      <div class="container">
+        <h2>Founding</h2>
+        <div class="progress">
+          <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+            60%
+          </div>
+        </div>      </div><!-- /.container -->
+    </div><!-- /.row -->
+
+    <div class="row">
+      <div class="container">
+        <h2>About us</h2>
+        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum facilisis aliquet sodales. Nulla massa lacus, scelerisque sodales consectetur et, vestibulum non diam. Nulla ullamcorper eros non sem auctor mattis. Nullam faucibus lacinia lectus, ac consectetur est sagittis quis. Nullam sit amet sapien justo. In in lacinia quam, at aliquet ligula. Proin turpis lectus, finibus viverra nunc mollis, dignissim venenatis quam. Nulla et scelerisque ipsum. Donec aliquam aliquet elit eu ullamcorper. Nulla facilisi. Vestibulum mollis lacus ipsum, non accumsan arcu consectetur nec. Cras ante erat, consequat consequat mattis eget, aliquam sed dui. Ut sollicitudin tempus orci sed consequat. </p>
+        <h2>Contact</h2>
+        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum facilisis aliquet sodales. Nulla massa lacus, scelerisque sodales consectetur et, vestibulum non diam. Nulla ullamcorper eros non sem auctor mattis. Nullam faucibus lacinia lectus, ac consectetur est sagittis quis. Nullam sit amet sapien justo. In in lacinia quam, at aliquet ligula. Proin turpis lectus, finibus viverra nunc mollis, dignissim venenatis quam. Nulla et scelerisque ipsum. Donec aliquam aliquet elit eu ullamcorper. Nulla facilisi. Vestibulum mollis lacus ipsum, non accumsan arcu consectetur nec. Cras ante erat, consequat consequat mattis eget, aliquam sed dui. Ut sollicitudin tempus orci sed consequat. </p>
+      </div><!-- /.container -->
+    </div><!-- /.row -->
 
 
     <footer class="footer">
-      <div class="container">\
+      <div class="container">
       </div><!-- footer/.container -->
     </footer>
 
